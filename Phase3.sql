4c3cdcfb6205190e3650950d88479cb8d1a4e04c
Precioux <uni.mahdipour@gmail.com>
2023-01-25 17:39:09 +0330

    Phase 3 and 2

---
+++ Phase3.sql
@@ -0,0 +1,15 @@
+INSERT INTO `sys`.`supplier` (`supplierID`, `supplier_name`, `contract_start`, `contract_finish`, `ceo_name`, `addressF`) VALUES ('1', 'Maggie ', '2001-02-01', '2002-02-01', 'Margaret', 'Pinnickinick Street, Tigard, WA');
+INSERT INTO `sys`.`supplier` (`supplierID`, `supplier_name`, `contract_start`, `contract_finish`, `ceo_name`, `addressF`) VALUES ('2', 'McClintock', '2000-11-18', '2005-01-10', 'Rosa ', 'Beeghley Street, Proctor, TX');
+INSERT INTO `sys`.`supplier` (`supplierID`, `supplier_name`, `contract_start`, `contract_finish`, `ceo_name`, `addressF`) VALUES ('3', 'Adams', '2020-03-15', '2022-10-16', 'Sherry', 'Adams Drive, Sugar Land, TX');
+INSERT INTO `sys`.`supplier` (`supplierID`, `supplier_name`, `contract_start`, `contract_finish`, `ceo_name`, `addressF`) VALUES ('4', 'York', '2008-09-14', '2020-01-25', 'Ann ', 'Croft Drive, Greeneville, TN');
+
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('1', 'Jessica ', 'Fountain', '516949275', 'Chicago Avenue, Fresno', '559-579-4363');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('2', 'Enos', 'Strosin ','439925925', 'Leuschke Lodg, New Martinamouth, LA', '956-522-2243');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('3', 'Delpha', 'Kozey', '504828550', 'Daniel View Apt. 749, Parkerbury, SD', '401-741-9623');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('4', 'Cheyanne','Schaden ', '455642046', 'Heaney Creek, Kaelaside, TX', '619-670-8183');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('5', 'Luella','Boyer ',  '479350319', 'Cleo Grove, Bashirianburgh, IA', '801-939-9885');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('6', 'Magnus', 'Lehner ', '435115064', 'Daniel Brooks Suite 537, North Duanehaven, LA', '205-484-6204');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('7', 'Dillon', 'Wolf ', '037344862', 'Simonis Tunnel Suite 936, Abbeyville, RI', '903-523-0656');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('8', 'Austen', 'Bruen', '216101764', 'Glover Pike Suite 593, Port Jacinto, MD', '803-802-5275');
+INSERT INTO `sys`.`customers` (`CustomerID`, `fname`, `lname`, `national_ID`, `addressC`, `phone`) VALUES ('9', 'Chasity', 'Gleason ', '585743418', 'Okuneva Passage Apt. 510, Wolfport, NM', '281-259-8519');
+
